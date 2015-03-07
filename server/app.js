d9d958dd74f074f2f343b90a34ac362fd100b905
zygimantas <zygimantasdev@gmail.com>
2015-03-08 00:52:24 +0200

    check auth after static

--- server/app.js
+++ server/app.js
@@ -9,17 +9,6 @@ var session  = require('express-session');
 
 var app = module.exports.app = exports.app = express();
 
-function checkAuth (req, res, next) {
-    if (req.url !== '/login') {
-        if(!req.session.userId) {
-            res.send('Unauthorized');
-            return;
-        }
-    }
-    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-    next();
-}
-
 //error handling
 app.use(function(err, req, res, next) {
     console.error(err.stack);
@@ -36,7 +25,6 @@ app.use(session({
     saveUninitialized: true,
     secret: 'keyboard_cat'
 }));
-app.use(checkAuth);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
--- server/app/routes/routes.js
+++ server/app/routes/routes.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function (app) {
+    function checkAuth (req, res, next) {
+        if (req.url !== '/login' && req.url !== '/') {
+            if(!req.session.userId) {
+                res.send('Unauthorized');
+                return;
+            }
+        }
+        res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+        next();
+    }
+    app.use(checkAuth);
+
+
     app.get('/', function (req, res) {
         res.render('index.html')
     });
